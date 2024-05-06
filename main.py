774df5707ea4f6176de500740eba10c6234850ad
Juangithubgit <88512947+Juangithubgit@users.noreply.github.com>
2024-05-06 09:25:55 -0400

    Add files via upload
    4/5/24
    Juan
    Created a dictionary of words assigned to strings that are assigned to functions ex: “show: print”, created two empty text files, one will be read and translated and the other executed. Created functions for random string generation and socket connection(most likely does not work?).
    4/8/24
    Juan
    Utilized file reading logic from hangman game to read the “read file”. Attempted to create a parser that will read through the file’s text and translate it using the dictionary 
    4/15/24
    Juan 
    Figured out how to fix parser by using .replace(). Created a loop for texting reasons whereupon any input the code in the “read” file is translated using the parser then pasted into a do file which is read and executed.
    5/2/24
    Juan
    Added more functions such as encryption and decryption using unicode offsets based on a random offset. Added these functions to the dictionary. 
    5/3/24
    Juan
    Created and added project to github repository. Added more math and boolean related translations to the dictionary.
    5/6/24
    Juan
    Created a new python file for testing reasons. Created a new function that will dynamically create a graph.

---
+++ main.py
@@ -0,0 +1,182 @@
+import random
+import string
+import turtle
+# import pygame
+import time
+import math
+
+
+dumbo_list_of_things = {
+    'run_if': 'while',
+    'show': 'print',
+    'do_this': ':',
+    'then_do': ':',
+    'define': 'def',
+    "go": "pass",
+    "generate_rstring": "grw",
+    "send_to": "s_connect",
+    "connect_to": "c_connect",
+    "random_hide": "encrypt",
+    'unhide': "decrypt",
+    'greater_than': '>',
+    'less_than': "<",
+    '=or_greater_then': '>=',
+    'create_graph': "make_graph"
+
+
+}
+
+
+def make_graph(title, rows , columns):
+    row_names = []
+    for row in rows:
+        row_names.append(row)
+    if len(row_names) <= 5:
+        print("----------------------------------------")
+        print("                " + title)
+    else:
+        print("------------------------------------------------------------------------------------")
+        print("                " + title)
+    for name in row_names:
+        print("    " + name, end = ' ')
+    print()
+    for column in columns:
+        print(column)
+
+
+
+
+
+
+
+
+user = None
+arg = False
+read_this = ''
+argument = ''
+conversions = []
+do = ''
+rip = []
+
+offset_value = random.randint(1, 5)
+
+
+def randomized():
+    return random.randint(1, 5) * random.randint(1, 9)**random.randint(0, 5)-random.randint(2, 30)
+
+
+def offset(letter):
+    unicode = ord(letter)
+    letter = chr(unicode + offset_value)
+    return letter
+
+
+def reverse_offset(letter):
+    unicode = ord(letter)
+    letter = chr(unicode - offset_value)
+    return letter
+
+
+def encrypt(data):
+    join_this = []
+    return_this = None
+    try:
+        data = data.spilt()
+    except Exception as e:
+        data = data
+    for i in range(len(data)):
+        for character in data[i]:
+            join_this.append(offset(data[i]))
+    return_this = ''.join(join_this)
+    return return_this
+
+
+def decrypt(data):
+    join_this = []
+    return_this = None
+    try:
+        data = data.spilt()
+    except Exception as e:
+        data = data
+    for i in range(len(data)):
+        for character in data[i]:
+            join_this.append(reverse_offset(data[i]))
+    return_this = ''.join(join_this)
+    return return_this
+
+
+def grw(t):
+    blank = ''.join(random.choice(string.ascii_lowercase) for _ in range(t))
+    return blank
+
+
+def s_connect(port):
+    import socket
+
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    soc.bind(("0.0.0.0", port))
+    soc.listen(5)
+
+    while True:
+        client_socket, client_address = soc.accept()
+        print(f"Connection from {client_address} has been established.")
+        sending = True
+        while sending:
+            data = client_socket.recv(1024)
+            if data:
+                print("DATA: ", data)
+            else:
+                print("No data sent")
+            sent = input("SEND: ")
+            client_socket.send(sent.encode())
+
+
+def c_connect(port):
+    import socket
+
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    soc.bind(("0.0.0.0", port))
+    soc.listen(5)
+
+    while True:
+        client_socket, client_address = soc.accept()
+        print(f"Connection from {client_address} has been established.")
+        sending = True
+        while sending:
+            data = client_socket.recv(1024)
+            if data:
+                print("DATA: ", data)
+
+            else:
+                print("No data sent")
+            sent = input("SEND: ")
+            client_socket.send(sent.encode())
+
+
+def turn():
+    open('do_this', "w").close()
+    file = open('read', "r")
+    data = []
+    for w in file:
+        data.append(w.rstrip())
+    for i in range(len(data)):
+        for thing in dumbo_list_of_things:
+            if thing in data[i]:
+                key = dumbo_list_of_things.get(thing)
+                data[i] = data[i].replace(thing, key)
+
+    for tok in data:
+        with open('do_this', 'a') as f:
+            f.write(tok + "\n")
+    with open('do_this', 'r') as fi:
+        code = fi.read()
+    exec(code)
+
+
+while user != "q":
+    user = input("> ")
+    try:
+        turn()
+        offset_value = randomized()
+    except Exception as e:
+        print(e)
